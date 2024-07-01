b58177ebe51b996e3905eb2f8559087f9f5b0b17
davipraciano <173275146+davipraciano@users.noreply.github.com>
2024-07-01 19:04:05 -0300

    up

---
+++ ex01/assents/js/script.js
@@ -0,0 +1,26 @@
+let nome = "Davi";
+let sobrenome = "Almeida";
+let idade = 15;
+let estado = "Rio Grande do Sul"
+let capital = "Porto Alegre"
+let cidade = "Bento Gonçalves";
+let escola = "Meste Santa Barbara";
+
+let nomecompleto = `Nome: ${nome} ${sobrenome}`;
+
+let Idade = `Idade: ${idade}`;
+
+let Estado = `Estado: ${estado}`;
+
+let Capital = `Capital: ${capital}`;
+
+let Cidade = `Cidade: ${cidade}`;
+
+let Escola = `Escola: ${escola}`
+
+console.log(nomecompleto)
+console.log(Idade)
+console.log(Estado)
+console.log(Capital)
+console.log(Cidade)
+console.log(Escola)
---
+++ ex02/script.js
@@ -0,0 +1,24 @@
+alert("Estás configurações ja estão sendo usadas!")
+let nome = "Paulo";
+let sobrenome = "Almeida";
+let idade = 49;
+let estadonasc = "Pernambuco";
+let cidadenasc = "Recife";
+let interiorec = "Lagoa do Itaenga";
+let trabalho = "Balconista";
+
+let logado = false
+
+let nomecompleto = `Nome: ${nome} ${sobrenome}`;
+let idade2 = `Idade: ${idade}`;
+let estadodenasc = `Estado de nascimento: ${estadonasc}`;
+let cidadedenasc = `Cidade de nascimento: ${cidadenasc}`;
+let interioderec = `Interior do Estado: ${interiorec}`;
+let trabalho2 = `Está trabalhando atualmente como: ${trabalho}`;
+
+console.log(nomecompleto);
+console.log(idade2);
+console.log(estadodenasc);
+console.log(cidadedenasc);
+console.log(interioderec);
+console.log(trabalho2);
