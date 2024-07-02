69685d2eb54fc151faa74465e58f1450c5dbca3a
davipraciano <173275146+davipraciano@users.noreply.github.com>
2024-07-02 08:40:02 -0300

    up

---
+++ ex05/js.js
@@ -0,0 +1,10 @@
+let idade = 18
+
+
+if (idade < 18) {
+    console.log("Você é uma adolescente.")
+} else if (idade >= 18 && idade <= 60) {
+    console.log("Você é um adulto.")
+} else if (idade >= 60) {
+    console.log("Você é um idoso.")
+}
