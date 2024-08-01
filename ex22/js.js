c1a89e86ccf2fd120556314e61d3d1da33d242ec
davipraciano <173275146+davipraciano@users.noreply.github.com>
2024-07-31 21:02:07 -0300

    up

---
+++ ex22/js.js
@@ -0,0 +1,25 @@
+let pessoa = {
+    nome: "Davi",
+    cabelo: "Preto",
+    olho: "Marrom",
+    altura: 1.72,
+    pe: 43,
+    caracteristica: [
+        "Estudante",
+        "Gamer",
+        "Programador",
+        "Cristão"
+    ]
+}
+
+console.log(`Nome: ${pessoa.nome}`);
+console.log(`   Status  `);
+console.log(`Cabelo: ${pessoa.cabelo}`);
+console.log(`Olho: ${pessoa.olho}`);
+console.log(`Altura: ${pessoa.altura}`);
+console.log(`Tamanho do Pé: ${pessoa.pe}`);
+console.log(`   Caracteristica   `);
+console.log(`${pessoa.caracteristica[0]}`);
+console.log(`${pessoa.caracteristica[1]}`);
+console.log(`${pessoa.caracteristica[2]}`);
+console.log(`${pessoa.caracteristica[3]}`);
---
+++ ex23/js.js
@@ -0,0 +1,10 @@
+let personagem = {
+    nome: "Bonieky",
+    idade: 90,
+    carros: [
+        {modelo: "Fiat", cor: "preto"},
+        {modelo: "Ferrari", cor: "vermelho"}
+    ]
+}
+
+console.log(personagem.carros[1].modelo)
