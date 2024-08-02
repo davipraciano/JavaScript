c9343dda32ce260e2625a8be394a2a7e0c834538
davipraciano <173275146+davipraciano@users.noreply.github.com>
2024-08-02 18:41:40 -0300

    up

---
+++ ex24/js.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome: "Bonieky",
+    sobrenome: "Lacerda",
+    idade: 90,
+    nomeCompleto: function () {
+        return `${this.nome} ${this.sobrenome}`
+    }
+}
+
+console.log(pessoa.nomeCompleto())
+
