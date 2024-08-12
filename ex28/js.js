006a411b5e56466fdcb6247146216ca133a5e440
davipraciano <173275146+davipraciano@users.noreply.github.com>
2024-08-12 20:03:40 -0300

    exercício de loop

---
+++ ex28/js.js
@@ -0,0 +1,15 @@
+// 1. Faça um loop que mostre todas as fruta
+let fruits = ["Maçã", "Uva", "Banana"]
+
+for(let fruit in fruits) {
+    console.log(`${fruits[fruit]}`)
+}
+
+// 2. Conte de 1 até 100 através de um WHILE
+
+let numero = 0
+
+while(numero <= 100) {
+    console.log(`${numero}`)
+    numero++
+}
