4de42efef90cd40ab0925f0e153ef2f1a22aa2d0
davipraciano <173275146+davipraciano@users.noreply.github.com>
2024-08-14 20:44:50 -0300

    funções em array

---
+++ ex29/js.js
@@ -0,0 +1,11 @@
+let fruits = ["Maçã", "Uva", "Laranja", "Banana"]
+fruits.pop()
+fruits.push("Pêra")
+console.log(fruits.join(", "))
+
+
+//lenght = quantidade de itens no array
+//push = adiciona itens no array
+//pop = deleta o ultimo item no array
+//shift = deleta o primeiro item no array
+//join = pega o array e coloca no console o que colocou no join
