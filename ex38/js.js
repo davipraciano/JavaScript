d3a9a1c81849a2f0276e5c427e9c2800bf4332c3
davipraciano <173275146+davipraciano@users.noreply.github.com>
2024-09-04 23:00:27 -0300

    eventos de teclado 1

---
+++ ex38/js.js
@@ -0,0 +1,13 @@
+function apertou() {
+    console.log("apertou")
+}
+
+function segurou() {
+    console.log("segurou")
+}
+
+function soltou() {
+    console.log("soltou")
+}
+    
+document.addEventListener("keyup", soltou)
