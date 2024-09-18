e322493c1426183ec90f012699434cde52809027
davipraciano <173275146+davipraciano@users.noreply.github.com>
2024-09-18 13:50:32 -0300

    Classes: Variáveis/Métodos estáticos

---
+++ ex45/js.js
@@ -0,0 +1,16 @@
+class Person {
+
+    static hands = 2
+    age = 0
+
+        constructor(name) {
+            this.name = name
+        }
+
+        sayHi() {
+            console.log(`Oi eu sou ${this.name} e tenho ${Person.hands} mãos`)
+        }
+}
+
+let p1 = new Person("Davi")
+p1.sayHi()
