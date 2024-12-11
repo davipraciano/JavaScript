f9935e427270bbaade9d67fcf72d3fd1987d75b3
davipraciano <173275146+davipraciano@users.noreply.github.com>
2024-12-11 19:14:58 -0300

    Métodos String (1/3)

---
+++ ex53/script.js
@@ -0,0 +1,11 @@
+let nome = "Davi Almeida Leal";
+
+let resultado = ''
+
+if (nome.indexOf('Almeida') > -1) {
+    resultado = 'achou'
+} else {
+    resultado = 'não achou'
+}
+
+console.log(resultado)
